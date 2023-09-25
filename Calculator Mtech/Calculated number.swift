181c0d40c90903cb2d101dc115fc37335ad1fad2
spoons <ev4n.mus1c@gmail.com>
2023-09-24 23:09:46 -0600

    Initial Commit

---
+++ Calculator Mtech/Calculated number.swift
@@ -0,0 +1,8 @@
+//
+//  Calculated number.swift
+//  Calculator Mtech
+//
+//  Created by shark boy on 9/24/23.
+//
+
+import Foundation
---
+++ Calculator Mtech/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator Mtech
+//
+//  Created by shark boy on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var math = Double()
+    
+    @IBOutlet weak var number: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func one(_ sender: UIButton) {
+//        if number.title == 0 {
+            
+        }
+    }
+    
+//}
+
